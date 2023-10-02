c946f6d931a0698dd0158a3b6178d6ec6ecc9a26
deeper747 <pisces747@gmail.com>
2023-10-02 12:23:28 -0400

    Filter record into dictionary

--- ARGOSTrackingTool.py
+++ ARGOSTrackingTool.py
@@ -41,6 +41,8 @@ for lineString in line_list:
     obs_lat = lineData[6]
     obs_lon = lineData[7]
 
-    #Add items to dictionary
-    date_dict[record_id] = obs_date
-    location_dict[record_id] = (obs_lat,obs_lon)
+    #Determine if location criterion is met
+    if obs_lc in ("1","2","3"):
+        #Add items to dictionary
+        date_dict[record_id] = obs_date
+        location_dict[record_id] = (obs_lat,obs_lon)
